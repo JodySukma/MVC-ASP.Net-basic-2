d66724e96391875e5103b9bc4c411bfbda277f03
lJodyl <zody.kyozue@gmail.com>
2019-04-22 15:16:40 +0700

    first commit

---
+++ SoalKe-2/Controllers/HitungController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SoalKe_2.Models;
+
+namespace SoalKe_2.Controllers
+{
+    public class HitungController : Controller
+    {
+        [HttpGet]
+        public IActionResult Hitung()
+        {
+            PersegiPanjang pp = new PersegiPanjang();
+            return View(pp);
+        }
+
+        [HttpPost]
+        public IActionResult Hitung(PersegiPanjang pp)
+        {
+            return View(pp);
+        }
+
+    }
+}
---
+++ SoalKe-2/Models/PersegiPanjang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoalKe_2.Models
+{
+    public class PersegiPanjang
+    {
+        //private double _panjang;
+        //private double _lebar;
+        //private double _hitungLuas;
+        //private double _hitungKeliling;
+
+        //public double Panjang { get => _panjang; set => _panjang = value = 0; }
+        //public double Lebar { get => _lebar; set => _lebar = value = 0; }
+        //public double HitungLuas {
+        //    get => _hitungLuas;
+        //    set => _hitungLuas = _panjang * _lebar;
+        //}
+        //public double HitungKeliling {
+        //    get => _hitungKeliling;
+        //    set => _hitungKeliling = 2 * (_panjang + _lebar);
+        //}
+        public double Panjang { get; set; } = 0;
+        public double Lebar { get; set; } = 0;
+        public double Luas => Panjang * Lebar;
+        public double Keliling => 2 * (Panjang + Lebar);
+    }
+}
